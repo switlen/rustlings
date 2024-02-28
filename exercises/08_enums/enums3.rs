596ff688e7aa749bfafc5943df20053447e20436
switlen <gd993915@163.com>
2024-02-28 23:46:29 +0800

    complete  exercises 08

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit
 }
 
 struct Point {
@@ -44,6 +48,23 @@ impl State {
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
         // fn function((t, u, p, l, e))
+        match message {
+            Message::Echo(s) => {
+                self.echo(s);
+            },
+            Message::ChangeColor(x, y, z) =>{
+                self.change_color((x,y,z));
+            },
+            Message::Move(point) => {
+                self.move_position(point);
+            },
+            Message::Quit =>{
+                self.quit();
+            },
+            _ => {
+
+            }
+        }
     }
 }
 
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(price: i32) -> i32{
+    if (price > 40) {
+        price
+    } else {
+        price * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
@@ -31,3 +37,5 @@ fn verify_test() {
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
+
+
