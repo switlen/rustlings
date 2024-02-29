d45cb937786d789d5721a280f3c003dbecd74582
switlen <gd993915@163.com>
2024-02-29 23:38:35 +0800

    complete exercise 11

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -39,6 +39,14 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded by team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let mut tm1 = scores.entry(team_1_name.clone()).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+        tm1.goals_scored+=team_1_score;
+        tm1.goals_conceded+=team_2_score;
+
+        let mut tm2 = scores.entry(team_2_name.clone()).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+        tm2.goals_scored+=team_2_score;
+        tm2.goals_conceded+=team_1_score;
+
     }
     scores
 }
